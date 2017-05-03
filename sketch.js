eb6cffd5d2c04dfb9f1e2bf5023b972886fa22e2
mottlowitzmk <mottlowitzmk@mx.lakeforest.edu>
2017-05-03 16:09:10 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,59 +1,38 @@
-//the setup function is performed once when the sketch loads.
+var cnv;
+
+var x = 50,
+  y = 50,
+  angle1 = 0.0,
+  segLength = 50;
+
+
 function setup() {
-    createCanvas(2000,2000);
+cnv = createCanvas(2000,100);
+cnv.position(20, 25);
+cnv.style("z-index", -1);
 
+  strokeWeight(20.0);
+  stroke(255, 100);
 
+}
 
 function draw() {
-  var r1, r2, r3, r4;
+  background(20,20,10,0);
 
-  function setup() {
-    createCanvas(710, 400);
-    fill(255, 204);
-    noStroke();
-    r1 = new MRect(1, 134.0, 0.532, 0.1*height, 10.0, 60.0);
-    r2 = new MRect(2, 44.0, 0.166, 0.3*height, 5.0, 50.0);
-    r3 = new MRect(2, 58.0, 0.332, 0.4*height, 10.0, 35.0);
-    r4 = new MRect(1, 120.0, 0.0498, 0.9*height, 15.0, 60.0);
-  }
 
-  function draw() {
-    background(0);
+  dx = mouseX - x;
+  dy = mouseY - y;
+  angle1 = atan2(dy, dx);
+  x = mouseX - (cos(angle1) * segLength);
+  y = mouseY - (sin(angle1) * segLength);
 
-    r1.display();
-    r2.display();
-    r3.display();
-    r4.display();
+  ellipse(x, y, 25, 25);
+}
 
-    r1.move(mouseX-(width/2), mouseY+(height*0.1), 30);
-    r2.move((mouseX+(width*0.05))%width, mouseY+(height*0.025), 20);
-    r3.move(mouseX/4, mouseY-(height*0.025), 40);
-    r4.move(mouseX-(width/2), (height-mouseY), 50);
-  }
-
-  function MRect(iw, ixp, ih, iyp, id, it) {
-    this.w = iw; // single bar width
-    this.xpos = ixp; // rect xposition
-    this.h = ih; // rect height
-    this.ypos = iyp; // rect yposition
-    this.d = id; // single bar distance
-    this.t = it; // number of bars
-
-    this.move = function(posX, posY, damping) {
-      var dif = this.ypos - posY;
-      if (abs(dif) > 1) {
-        this.ypos -= dif/damping;
-      }
-      dif = this.xpos - posX;
-      if (abs(dif) > 1) {
-        this.xpos -= dif/damping;
-      }
-    }
-
-    this.display = function() {
-      for (var i=0; i<this.t; i++) {
-        rect(this.xpos+(i*(this.d+this.w)),
-          this.ypos, this.w, height*this.h);
-      }
-    }
-  }
+function segment(x, y, a) {
+  push();
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+  pop();
+}
